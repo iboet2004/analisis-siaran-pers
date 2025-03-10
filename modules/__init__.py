4020ac6a0aa9ed326d63dba8b70e7aa95bf0bd53
iboet2004 <iboet2004@gmail.com>
2025-03-10 11:13:14 +0700

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -5,9 +5,9 @@ Analisis Siaran Pers Modules Package.
 # Import modules to make them available when importing the package
 from .document_processor import DocumentProcessor
 from .keyword_extractor import KeywordExtractor
+from .news_finder import NewsFinder
 
 # Modules yang akan diimplementasikan kemudian
-# from .news_finder import NewsFinder
 # from .sentiment_analyzer import SentimentAnalyzer
 # from .visualizer import Visualizer
 
