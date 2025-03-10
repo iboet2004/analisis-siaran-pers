ef4803341760dba7a895d9ce07d4beabdbc8d666
iboet2004 <iboet2004@gmail.com>
2025-03-10 09:48:44 +0700

    Create __init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,14 @@
+"""
+Analisis Siaran Pers Modules Package.
+"""
+
+# Import modules to make them available when importing the package
+from .document_processor import DocumentProcessor
+
+# Modules yang akan diimplementasikan kemudian
+# from .keyword_extractor import KeywordExtractor
+# from .news_finder import NewsFinder
+# from .sentiment_analyzer import SentimentAnalyzer
+# from .visualizer import Visualizer
+
+__all__ = ['DocumentProcessor']  # Tambahkan modul lain di sini nanti
